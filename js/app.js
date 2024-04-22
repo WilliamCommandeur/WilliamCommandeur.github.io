de818ef3a91f84019f543a25e1beefc55fbdd5f9
WilliamCommandeur <william.commandeur@gmail.com>
2024-04-22 14:54:38 +0200

    style(about page)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var title = document.getElementById("fade-content");
-    title.style.opacity = 1;
+const animations = [
+  { selector: '.animate-on-load-first', delay: 500 },
+  { selector: '.animate-on-load-second', delay: 1200 },
+  { selector: '.animate-on-load-third', delay: 1900 }
+];
+
+function applyAnimation(selector, delay) {
+  const elementsToAnimate = document.querySelectorAll(selector);
+
+  elementsToAnimate.forEach(function(element) {
+      setTimeout(function() {
+          element.style.opacity = '1';
+      }, delay);
   });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  animations.forEach(function(animation) {
+    applyAnimation(animation.selector, animation.delay);
+  });
+  
+  const flashingElements = document.querySelectorAll('.animate-light');
+  flashingElements.forEach(function(element) {
+    setTimeout(function() {
+        element.style.opacity = '1';
+    }, 4000); 
+  });
+
+});
