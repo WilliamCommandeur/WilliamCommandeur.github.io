a6ac6d059b8aeea1ae4d6abf160bfaeedc173f66
WilliamCommandeur <william.commandeur@gmail.com>
2024-04-24 11:26:48 +0200

    add responsiveness with modal and slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,14 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 80,
+    breakpoints: {
+        800: {
+            slidesPerView:2
+        },
+        1200: {
+            slidesPerView:3
+        }
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
