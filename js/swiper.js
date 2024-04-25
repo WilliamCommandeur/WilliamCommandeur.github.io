058c014b5a4db9e5fb76e77366d94e97df8d3e2f
WilliamCommandeur <william.commandeur@gmail.com>
2024-04-25 11:12:55 +0200

    add responsiveness for tablet

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,23 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 80,
+    spaceBetween: 100,
+    
     breakpoints: {
-        800: {
-            slidesPerView:2
+        850: {
+            slidesPerView:2,
+            spaceBetween: 80,
         },
         1200: {
-            slidesPerView:3
+            slidesPerView:3,
+            spaceBetween: 80
         }
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+    }
 });
